1afc52c42cd73cf952bae7c82bee50c4ccaf9b83
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-09 09:39:01 +0900

    Add react-native-appwrite dependency and initialize Appwrite client

---
+++ lib/appwrite.ts
@@ -0,0 +1,25 @@
+import { Account, Avatars, Client } from "react-native-appwrite";
+
+export const config = {
+  platform: "com.dv.realestate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function Login() {
+  try {
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
